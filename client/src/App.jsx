1b2d70d3d149950af816dbfdae522054e48c37da
josh manansala <joshmanansalagsd@gmail.com>
2024-02-01 16:14:18 -0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+      </header>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </main>
+  );
+}
+
+export default App;
