9c6962bf7e253e5028ec09e6d1c72becc937c4fa
josh manansala <joshmanansalagsd@gmail.com>
2024-02-19 15:36:30 -0800

    feature: profile controller verify token to check if useris logged in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const Header = () => {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/auth/profile", {
+      credentials: "include",
+    });
+  }, []);
   return (
     <header>
       <Link to="/" className="logo">
