0842c6aefe20162c7b30fcc7940c95d175c81ec1
josh manansala <joshmanansalagsd@gmail.com>
2024-02-19 17:21:42 -0800

    feature: logout user controller

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, profile } = require("../controllers/auth.js");
+const { register, login, profile, logout } = require("../controllers/auth.js");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", profile);
+router.post("/logout", logout);
 
 module.exports = router;
