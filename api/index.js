9495d07dc85f7dd022d04d6389b3fd74ffdf483c
josh manansala <joshmanansalagsd@gmail.com>
2024-03-28 12:51:09 -0700

    test: credentials to fix cors err for login and registration

--- api/index.js
+++ api/index.js
@@ -15,7 +15,11 @@ const app = express();
 // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 // TESTING
 // app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 // DEPLOY
 // app.use(
 //   cors({
